d32d27ab529d318e4da095273b7432ea62247323
Luiz <lhvalentim@outlook.coom>
2022-04-04 12:18:42 -0300

    rest timer

--- precache-manifest.8cc9b0e453ed46bbd461d13a34229daa.js
+++ precache-manifest.8cc9b0e453ed46bbd461d13a34229daa.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "07b169e053704e4d8b19",
-    "url": "/academinha/css/app~d0ae3f07.e6b91ff2.css"
+    "revision": "7306b65187fb5eba2c6f",
+    "url": "/academinha/css/app~d0ae3f07.aaf30bd0.css"
   },
   {
     "revision": "12119734b4a5002f7e1f",
@@ -10,6 +10,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "adb244ec74addaf0da40",
     "url": "/academinha/css/chunk-vendors~25d43e53.51b8982f.css"
+  },
+  {
+    "revision": "9d57b645ff08bd80913dc67c707fbf07",
+    "url": "/academinha/ding.aac"
+  },
+  {
+    "revision": "e0155a7e9d4cc4b27fd16f58f503d4d1",
+    "url": "/academinha/ding.ogg"
   },
   {
     "revision": "4a25e1103d02c98b79080f57b60bcdc9",
@@ -40,20 +48,32 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/academinha/img/6.png"
   },
   {
-    "revision": "3cfbc09e67d25dce709e51051324df4e",
+    "revision": "708a8f739d0f9e201cebd5be9d894b1d",
     "url": "/academinha/index.html"
   },
   {
-    "revision": "07b169e053704e4d8b19",
-    "url": "/academinha/js/app~d0ae3f07.66d43cd4.js"
+    "revision": "7306b65187fb5eba2c6f",
+    "url": "/academinha/js/app~d0ae3f07.f443049b.js"
+  },
+  {
+    "revision": "ad980a4232755719a692",
+    "url": "/academinha/js/chunk-vendors~0a56fd24.2a8963e9.js"
   },
   {
     "revision": "12119734b4a5002f7e1f",
     "url": "/academinha/js/chunk-vendors~11c2601a.31e096f4.js"
   },
   {
+    "revision": "d04720834bf10db8344f",
+    "url": "/academinha/js/chunk-vendors~15f0789d.70768201.js"
+  },
+  {
     "revision": "adb244ec74addaf0da40",
     "url": "/academinha/js/chunk-vendors~25d43e53.2b69b084.js"
+  },
+  {
+    "revision": "a282ae95e9a7387a0a70",
+    "url": "/academinha/js/chunk-vendors~399b027d.9939d8aa.js"
   },
   {
     "revision": "c9ffb2b8892427800a07",
@@ -68,28 +88,44 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/academinha/js/chunk-vendors~77bf5e45.3ab28eda.js"
   },
   {
-    "revision": "19f6e4912f632b74297c",
-    "url": "/academinha/js/chunk-vendors~b1f96ece.fa8a091b.js"
+    "revision": "22812f1d1f8cd59444fb",
+    "url": "/academinha/js/chunk-vendors~8a7b4606.56508ba5.js"
+  },
+  {
+    "revision": "fb7fca18d0a3d2f836e7",
+    "url": "/academinha/js/chunk-vendors~8eeb4602.5da35ed0.js"
+  },
+  {
+    "revision": "f6d57663e126f0627838",
+    "url": "/academinha/js/chunk-vendors~987e6011.08f8512e.js"
+  },
+  {
+    "revision": "8f1ddeebf8cf33eceab2",
+    "url": "/academinha/js/chunk-vendors~b58f7129.71d196de.js"
+  },
+  {
+    "revision": "04b3b25dea3033cc27a0",
+    "url": "/academinha/js/chunk-vendors~bc9d1275.477163ae.js"
+  },
+  {
+    "revision": "2e1b47e1860c0046970f",
+    "url": "/academinha/js/chunk-vendors~d22b72d1.419889f1.js"
   },
   {
     "revision": "f3f17cb4dbddf5f10a42",
     "url": "/academinha/js/chunk-vendors~d2305125.556e3034.js"
   },
   {
-    "revision": "0ce585f625f625de485f",
-    "url": "/academinha/js/chunk-vendors~d939e436.ceff9eaa.js"
-  },
-  {
     "revision": "55415df756e7571d0e1b",
     "url": "/academinha/js/chunk-vendors~dff86cf2.acda773c.js"
   },
   {
-    "revision": "ca36f7daaa31078f7f6b",
-    "url": "/academinha/js/chunk-vendors~e4173fa2.4117b423.js"
+    "revision": "4bc92d513ab79972b79a",
+    "url": "/academinha/js/chunk-vendors~e258e298.12c97133.js"
   },
   {
-    "revision": "7503b01a2484554d999c",
-    "url": "/academinha/js/chunk-vendors~f9ca8911.aff0ac36.js"
+    "revision": "b3f99f589f2083f28afe",
+    "url": "/academinha/js/chunk-vendors~e4173fa2.bd44de04.js"
   },
   {
     "revision": "48e9a323061b5bb2032a",
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/academinha/precache-manifest.3f75164b2a344980f44c5e0db05a3174.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/academinha/precache-manifest.8cc9b0e453ed46bbd461d13a34229daa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({ prefix: 'academinha' })
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
