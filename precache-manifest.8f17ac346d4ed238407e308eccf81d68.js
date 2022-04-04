cb4e4fdd4f928fa378efcac665a796be0dbaef45
Luiz <lhvalentim@outlook.coom>
2022-04-04 10:08:02 -0300

    change day to training and play

---
+++ precache-manifest.8f17ac346d4ed238407e308eccf81d68.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e145c2deaf93f9bac7",
+    "url": "/academinha/css/app~d0ae3f07.6d22ad28.css"
+  },
+  {
+    "revision": "12119734b4a5002f7e1f",
+    "url": "/academinha/css/chunk-vendors~11c2601a.44f3e72d.css"
+  },
+  {
+    "revision": "adb244ec74addaf0da40",
+    "url": "/academinha/css/chunk-vendors~25d43e53.51b8982f.css"
+  },
+  {
+    "revision": "4a25e1103d02c98b79080f57b60bcdc9",
+    "url": "/academinha/img/0.png"
+  },
+  {
+    "revision": "2a852aba5913776a89df450366a159eb",
+    "url": "/academinha/img/1.png"
+  },
+  {
+    "revision": "0ee0485923c0a67b1c7129c63324bded",
+    "url": "/academinha/img/2.png"
+  },
+  {
+    "revision": "e641fc68b7074f6fa5b5e171702dd3f3",
+    "url": "/academinha/img/3.png"
+  },
+  {
+    "revision": "84720dc29e08f72f57dbccba03f307de",
+    "url": "/academinha/img/4.png"
+  },
+  {
+    "revision": "f5d194fbb5012f9d59b30f2323c2e042",
+    "url": "/academinha/img/5.png"
+  },
+  {
+    "revision": "071cd5d6bc061ed6c65a806d6ce9ad8e",
+    "url": "/academinha/img/6.png"
+  },
+  {
+    "revision": "9bac091a7487deeed0c51be31b3647a0",
+    "url": "/academinha/index.html"
+  },
+  {
+    "revision": "43e145c2deaf93f9bac7",
+    "url": "/academinha/js/app~d0ae3f07.7c994fe0.js"
+  },
+  {
+    "revision": "12119734b4a5002f7e1f",
+    "url": "/academinha/js/chunk-vendors~11c2601a.31e096f4.js"
+  },
+  {
+    "revision": "adb244ec74addaf0da40",
+    "url": "/academinha/js/chunk-vendors~25d43e53.2b69b084.js"
+  },
+  {
+    "revision": "c9ffb2b8892427800a07",
+    "url": "/academinha/js/chunk-vendors~4018e926.6e7937cc.js"
+  },
+  {
+    "revision": "b8607fe669fe9f02c414",
+    "url": "/academinha/js/chunk-vendors~4a7e9e0b.2cd1f7fd.js"
+  },
+  {
+    "revision": "72c423cc3e64479f09f6",
+    "url": "/academinha/js/chunk-vendors~77bf5e45.3ab28eda.js"
+  },
+  {
+    "revision": "19f6e4912f632b74297c",
+    "url": "/academinha/js/chunk-vendors~b1f96ece.fa8a091b.js"
+  },
+  {
+    "revision": "f3f17cb4dbddf5f10a42",
+    "url": "/academinha/js/chunk-vendors~d2305125.556e3034.js"
+  },
+  {
+    "revision": "0ce585f625f625de485f",
+    "url": "/academinha/js/chunk-vendors~d939e436.ceff9eaa.js"
+  },
+  {
+    "revision": "55415df756e7571d0e1b",
+    "url": "/academinha/js/chunk-vendors~dff86cf2.acda773c.js"
+  },
+  {
+    "revision": "ca36f7daaa31078f7f6b",
+    "url": "/academinha/js/chunk-vendors~e4173fa2.4117b423.js"
+  },
+  {
+    "revision": "7503b01a2484554d999c",
+    "url": "/academinha/js/chunk-vendors~f9ca8911.aff0ac36.js"
+  },
+  {
+    "revision": "48e9a323061b5bb2032a",
+    "url": "/academinha/js/chunk-vendors~fdc6512a.6c75db60.js"
+  },
+  {
+    "revision": "a923c426ad0f67d18bcd858f15c0d3c8",
+    "url": "/academinha/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/academinha/robots.txt"
+  }
+]);
