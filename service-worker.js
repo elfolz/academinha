cfb6f26ea56efa8575367f46edabcede01d023df
Luiz <lhvalentim@outlook.coom>
2022-04-05 13:08:05 -0300

    remove public path

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/academinha/precache-manifest.b094287d67a5f240316a13b9e86d1a9c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.93ec8b22fb974fc98c04bb239270d055.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({ prefix: 'academinha' })
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
